b5b4e8aba253cee3c2312d9792e63171dc3f2e81
mike-axiom-mir <miketobi90@gmail.com>
2026-09-15 21:06:22 +0200

    Expose explicit occupancy API

--- src/axm_stickers/__init__.py
+++ src/axm_stickers/__init__.py
@@ -8,6 +8,10 @@ from .connections import (compile_connection_plan, save_connection_plan,
                           solve_connection_plan, validate_connection_plan)
 from .closures import (save_closed_connection_plan, validate_closure_set,
                        verify_loop_closures)
+from .occupancy import (compile_occupancy_plan, occupancy_profile, occupancy_slots,
+                        save_occupancy_plan, solve_occupancy_plan,
+                        validate_occupancy_library, validate_occupancy_plan,
+                        validate_occupancy_profile)
 
 __all__ = ['Registry', 'digest', 'instance', 'resolve', 'validate',
            'attachment_matrix', 'placement_2d', 'InterfaceCatalog',
@@ -16,4 +20,7 @@ __all__ = ['Registry', 'digest', 'instance', 'resolve', 'validate',
            'compile_connection_plan', 'save_connection_plan',
            'solve_connection_plan', 'validate_connection_plan',
            'save_closed_connection_plan', 'validate_closure_set',
-           'verify_loop_closures']
+           'verify_loop_closures', 'compile_occupancy_plan', 'occupancy_profile',
+           'occupancy_slots', 'save_occupancy_plan', 'solve_occupancy_plan',
+           'validate_occupancy_library', 'validate_occupancy_plan',
+           'validate_occupancy_profile']
