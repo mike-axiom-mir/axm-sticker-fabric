8335b4e87f1d6be9f0bbb961e08e7ce3c5f94d60
mike-axiom-mir <miketobi90@gmail.com>
2026-09-15 21:04:55 +0200

    Start explicit occupancy companion layer

---
+++ src/axm_stickers/occupancy.py
@@ -0,0 +1,6 @@
+"""Explicit multi-occupancy slots layered above named rigid interface ports."""
+
+OCCUPANCY_PROFILE='axm.sticker-occupancy-profile/v0.1'
+OCCUPANCY_LIBRARY='axm.sticker-occupancy-library/v0.1'
+OCCUPANCY_PLAN='axm.sticker-occupancy-plan/v0.1'
+OCCUPANCY_SOLUTION='axm.sticker-occupancy-solution/v0.1'
